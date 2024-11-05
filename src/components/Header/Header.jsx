63f1ee78abeb6be5f734f07ec49ac1e0eb4ee03b
Torbex01 <oluwatobiawonuga@gmail.com>
2024-11-05 08:42:16 +0100

    05/11/24

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
 const Header = () =>{
     return(
         <div>
@@ -6,7 +8,16 @@ const Header = () =>{
                 <Logo>
                     <h2>OLUWATOBI</h2>
                 </Logo>
-                
+                <Navs>
+                    <nav>About</nav>
+                    <nav>Projects</nav>
+                    <nav>Contacts</nav>
+                    <section>
+                        <h2><FaGithub /></h2>
+                        <h2> <FaSquareXTwitter /> </h2>
+                        <h2> <FaLinkedin /> </h2>
+                    </section>
+                </Navs>
             </Container> 
         </div>
     )
@@ -14,9 +25,59 @@ const Header = () =>{
 export default Header
 
 const Container = styled.div `
-    background-color: pink;
-    max-width: 85%;
+     @media (max-width: 900px) {
+   justify-content: space-evenly;
+   align-items: center;
+   
+     }
+    /* background-color: pink; */
+    max-width: 95%;
     margin: 0 auto;
-    min-height: 60px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 `
-const Logo = styled.div ``
+const Logo = styled.div `
+     @media (max-width: 900px) {
+        h2{
+            font-size: 20px;
+        }
+     }
+    color: #18195f;
+    h2{
+        font-weight: 900;
+        text-shadow: 3px 3px 4px #808080;
+    }
+
+`
+
+const Navs = styled.div `
+ @media (max-width: 900px) {
+   justify-content: space-evenly;
+   align-items: center;
+   width: 60%;
+    section{
+        width: 50%;
+        justify-content: space-evenly;
+    }
+     }
+/* background-color: blue; */
+height: inherit;
+
+width: 40%;
+display: flex;
+align-items: center;
+justify-content: space-evenly;
+
+color: #424242;
+font-size: 17px;
+font-weight: 600;
+
+section{
+    display: flex;
+    width: 30%;
+    justify-content: space-evenly;
+    
+}
+`
